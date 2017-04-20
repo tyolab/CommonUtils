7ac0c706cde0f61cc19528290cc5517ffd82776a
tyodev <deve@tyo.com.au>
2017-04-20 16:20:14 +1000

    debugging needed

--- src/main/java/au/com/tyo/data/ContentTypes.java
+++ src/main/java/au/com/tyo/data/ContentTypes.java
@@ -1,40 +1,80 @@
 package au.com.tyo.data;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by monfee on 13/4/17.
  */
 
 public class ContentTypes {
+    
+    private static final String[] VIDEO_EXTENSIONS = {
+        "mov",
+        "avi",
+        "mp4",
+        "qt",
+        "ogv",
+        "webm",
+        "flv",
+        "mkv",
+        "vob",
+        "mpg",
+        "mpeg",
+        "m4p",
+        "m4v"
+    };
+
+    private static final List VIDEO_EXTENSION_LIST = Arrays.asList(VIDEO_EXTENSIONS);
+
+    private static final String[] IMAGE_EXTENSIONS = {
+            "png",
+            "gif",
+            "jpg",
+            "jpeg"
+            // more here
+    };
+
+    private static final List IMAGE_EXTENSION_LIST = Arrays.asList(IMAGE_EXTENSIONS);
+
+    private static String extensionCheck(String ext) {
+        String fileExt = ext;
+        if (ext.charAt(0) == '.')
+            fileExt = ext.substring(1);
+        return ext;
+    }
 
     /**
-     * Only for Java 7+
      *
      * @param ext
      * @return
      */
     public static boolean isVideo(String ext) {
+        return isType(VIDEO_EXTENSION_LIST, ext);
+    }
+
+    /**
+     *
+     * @param ext
+     * @return
+     */
+    public static boolean isImage(String ext) {
+        return isType(IMAGE_EXTENSION_LIST, ext);
+    }
+
+    /**
+     *
+     * @param list
+     * @param ext
+     * @return
+     */
+    private static boolean isType(List list, String ext) {
         if (null == ext || ext.length() == 0)
             return false;
-        String fileExt = ext;
-        if (ext.charAt(0) == '.')
-            fileExt = ext.substring(1);
 
-        switch (fileExt.toLowerCase()) {
-            case "mov":
-            case "avi":
-            case "mp4":
-            case "qt":
-            case "ogv":
-            case "webm":
-            case "flv":
-            case "mkv":
-            case "vob":
-            case "mpg":
-            case "mpeg":
-            case "m4p":
-            case "m4v":
-                return true;
-        }
+        String fileExt = extensionCheck(ext).toLowerCase();
+
+        list.contains(fileExt);
         return false;
     }
 }
