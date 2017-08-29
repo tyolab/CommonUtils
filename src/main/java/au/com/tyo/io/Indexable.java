61d50f8958c497b4d0cb3f2b999f0a77b75ff58e
tyodev <deve@tyo.com.au>
2017-08-29 13:50:46 +1000

    add a new method in listitem interface

---
+++ src/main/java/au/com/tyo/io/Indexable.java
@@ -0,0 +1,12 @@
+package au.com.tyo.io;
+
+/**
+ * Created by Eric Tang (eric.tang@tyo.com.au) on 23/8/17.
+ */
+
+public interface Indexable {
+
+    int getIndex();
+
+    void setIndex(int index);
+}
