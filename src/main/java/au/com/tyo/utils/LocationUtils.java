be98f3d5c7c5ecf93e4a40edcd2635d66ec3489a
tyodev <deve@tyo.com.au>
2017-09-23 11:46:27 +1000

    location service updates

---
+++ src/main/java/au/com/tyo/utils/LocationUtils.java
@@ -0,0 +1,19 @@
+package au.com.tyo.utils;
+
+/**
+ * Created by Eric Tang (eric.tang@tyo.com.au) on 23/9/17.
+ */
+
+public class LocationUtils {
+
+    /**
+     * Calculate the distance between two GPS points
+     *
+     * @param location1
+     * @param locations2
+     * @return
+     */
+    public double distance(double[] location1, double[] locations2) {
+        return 0.0d;
+    }
+}
