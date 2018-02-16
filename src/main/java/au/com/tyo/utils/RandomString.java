e592ea5cf9f33c3322be6698c15ab8e4b2632768
tyodev <deve@tyo.com.au>
2018-02-17 09:49:53 +1000

    more work

--- src/main/java/au/com/tyo/utils/RandomString.java
+++ src/main/java/au/com/tyo/utils/RandomString.java
@@ -8,28 +8,48 @@ package au.com.tyo.utils;
 import java.util.Random;
 
 public class RandomString {
-	  private static final char[] symbols = new char[36];
 
-	  static {
-	    for (int idx = 0; idx < 10; ++idx)
-	      symbols[idx] = (char) ('0' + idx);
-	    for (int idx = 10; idx < 36; ++idx)
-	      symbols[idx] = (char) ('a' + idx - 10);
-	  }
+	private static RandomString instance;
 
-	  private final Random random = new Random();
+	private static final char[] symbols = new char[36];
 
-	  private final char[] buf;
-
-	  public RandomString(int length) {
-	    if (length < 1)
-	      throw new IllegalArgumentException("length < 1: " + length);
-	    buf = new char[length];
-	  }
-
-	  public String nextString() {
-	    for (int idx = 0; idx < buf.length; ++idx) 
-	      buf[idx] = symbols[random.nextInt(symbols.length)];
-	    return new String(buf);
-	  }
+	static {
+		for (int idx = 0; idx < 10; ++idx)
+		  symbols[idx] = (char) ('0' + idx);
+		for (int idx = 10; idx < 36; ++idx)
+		  symbols[idx] = (char) ('a' + idx - 10);
 	}
+
+	private static RandomString getInstance() {
+		if (null == instance)
+			instance = new RandomString();
+	}
+
+	private final Random random = new Random();
+
+	private char[] buf;
+
+	private int length;
+
+	public RandomString(int length) {
+		if (length < 1)
+		  throw new IllegalArgumentException("length < 1: " + length);
+
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public String nextString() {
+		for (int idx = 0; idx < buf.length; ++idx)
+		  buf[idx] = symbols[random.nextInt(symbols.length)];
+		return new String(buf);
+	}
+
+	public static String createStringInLength(int length) {
+
+	}
+}
+
+
