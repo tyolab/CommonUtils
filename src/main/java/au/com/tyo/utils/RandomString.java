3b7423305340246586f299ab2c4b02e557b500cd
tyodev <deve@tyo.com.au>
2018-02-18 11:11:57 +1000

    more refactoring work

--- src/main/java/au/com/tyo/utils/RandomString.java
+++ src/main/java/au/com/tyo/utils/RandomString.java
@@ -20,9 +20,10 @@ public class RandomString {
 		  symbols[idx] = (char) ('a' + idx - 10);
 	}
 
-	private static RandomString getInstance() {
+    private static RandomString getInstance() {
 		if (null == instance)
 			instance = new RandomString();
+		return instance;
 	}
 
 	private final Random random = new Random();
@@ -31,24 +32,48 @@ public class RandomString {
 
 	private int length;
 
+    public RandomString() {
+
+    }
+
 	public RandomString(int length) {
 		if (length < 1)
 		  throw new IllegalArgumentException("length < 1: " + length);
 
+		setLength(length);
+
+		initializeBuffer();
 	}
 
-	public void setLength(int length) {
+    public int getLength() {
+        return length;
+    }
+
+    private void initializeBuffer() {
+        buf = new char[length];
+    }
+
+    public void setLength(int length) {
 		this.length = length;
+		buf = null;
 	}
 
 	public String nextString() {
+	    if (buf == null)
+	        initializeBuffer();
+
 		for (int idx = 0; idx < buf.length; ++idx)
 		  buf[idx] = symbols[random.nextInt(symbols.length)];
 		return new String(buf);
 	}
 
 	public static String createStringInLength(int length) {
+        if (getInstance().getLength() != length) {
+            getInstance().setLength(length);
+            getInstance().initializeBuffer();
+        }
 
+        return getInstance().nextString();
 	}
 }
 
