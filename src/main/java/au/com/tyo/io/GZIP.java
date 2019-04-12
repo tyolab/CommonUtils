b6211defb0b6d41133a0dcaeaff37fca13c5e578
Eric Tang <eric.tang@tyo.com.au>
2019-04-12 22:05:28 +1000

    finally, fixed the bug in the base64 encoding/decoding with padding (now removed)

--- src/main/java/au/com/tyo/io/GZIP.java
+++ src/main/java/au/com/tyo/io/GZIP.java
@@ -43,14 +43,14 @@ public class GZIP {
 	}
 	
 	public static InputStream decompressStream(InputStream input) throws IOException {
-	     PushbackInputStream pb = new PushbackInputStream( input, 2 ); //we need a pushbackstream to look ahead
-	     byte [] signature = new byte[2];
-	     pb.read( signature ); //read the signature
-	     pb.unread( signature ); //push back the signature to the stream
-	     if( signature[ 0 ] == (byte) 0x1f && signature[ 1 ] == (byte) 0x8b ) //check if matches standard gzip magic number
-	       return new GZIPInputStream( pb );
-	     else 
-	       return pb;
+		PushbackInputStream pb = new PushbackInputStream(input, 2); //we need a pushbackstream to look ahead
+		byte[] signature = new byte[2];
+		pb.read(signature); //read the signature
+		pb.unread(signature); //push back the signature to the stream
+		if (signature[0] == (byte) 0x1f && signature[1] == (byte) 0x8b) { //check if matches standard gzip magic number
+			return new GZIPInputStream(pb);
+		}
+		return pb;
 	}
 
 	public static String decompress(byte[] compressed) throws IOException {
